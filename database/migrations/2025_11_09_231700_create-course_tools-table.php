fe66802775ae41d1ab9e65c752e1cf5ac0ffec27
itsmht <iamtalha.mht@gmail.com>
2025-11-10 00:21:25 +0100

    modified

--- database/migrations/2025_11_09_231700_create-course_tools-table.php
+++ database/migrations/2025_11_09_231700_create-course_tools-table.php
@@ -12,21 +12,13 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('course_tools', function (Blueprint $table) {
-        $table->bigIncrements('course_tool_id');
-        $table->unsignedBigInteger('course_id');
-        $table->unsignedBigInteger('tool_id');
-        $table->timestamps();
-
-        $table->foreign('course_id')
-            ->references('course_id')
-            ->on('courses')
-            ->onDelete('cascade');
-
-        $table->foreign('tool_id')
-            ->references('tool_id')
-            ->on('tools')
-            ->onDelete('cascade');
-});
+            $table->bigIncrements('course_tool_id');
+            $table->unsignedBigInteger('course_id');
+            $table->unsignedBigInteger('tool_id');
+            $table->foreign('course_id')->references('course_id')->on('courses');
+            $table->foreign('tool_id')->references('tool_id')->on('tools');
+            $table->timestamps();
+        });
     }
 
     /**
